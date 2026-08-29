b53d81d8d955db9d4c606ede3f8c07068430e5bd
KonopatskiiDev <konopatskii.a@gmail.com>
2026-08-29 20:55:07 +0700

    subscriptiontracker modified main file for cookie session

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.getHttpAdapter().getInstance().set('trust proxy', 1);
   app.use(cookieSession({
     keys: ['my-secret-key'],
     sameSite: 'none',
